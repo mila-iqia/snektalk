7a2cac586f3167deee2146c6c2c7ffaff41f2b2a
Olivier Breuleux <breuleux@gmail.com>
2021-02-24 16:37:19 -0500

    Use split.js for resizable panes

--- download-libs.sh
+++ download-libs.sh
@@ -15,3 +15,6 @@ rm package-lock.json
 
 wget https://requirejs.org/docs/release/2.3.6/minified/require.js
 mv require.js $ASSETS/lib/require.min.js
+
+wget https://cdnjs.cloudflare.com/ajax/libs/split.js/1.2.0/split.min.js
+mv split.min.js $ASSETS/lib/split.min.js
