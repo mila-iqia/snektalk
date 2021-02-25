146c65d15087ed85a7b96b167fe4c9e4832f9176
Olivier Breuleux <breuleux@gmail.com>
2021-02-25 01:01:36 -0500

    Use split-grid.js instead of split.js

--- download-libs.sh
+++ download-libs.sh
@@ -16,8 +16,8 @@ rm package-lock.json
 wget https://requirejs.org/docs/release/2.3.6/minified/require.js
 mv require.js $ASSETS/lib/require.min.js
 
-wget https://cdnjs.cloudflare.com/ajax/libs/split.js/1.2.0/split.min.js
-mv split.min.js $ASSETS/lib/split.min.js
+wget https://cdn.jsdelivr.net/npm/split-grid@1.0.9/dist/split-grid.min.js
+mv split-grid.min.js $ASSETS/lib/split-grid.min.js
 
 wget https://cdnjs.cloudflare.com/ajax/libs/fuse.js/3.4.6/fuse.min.js
 mv fuse.min.js $ASSETS/lib/fuse.min.js
